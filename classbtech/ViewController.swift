51b1c64c90226f7ebd659135c67b1dcd9a6a228d
PURAM SAI SURAJ <suryasai653@achiever-132.local>
2019-08-06 18:02:14 +0530

    Initial Commit

---
+++ classbtech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  classbtech
+//
+//  Created by PURAM SAI SURAJ on 8/6/19.
+//  Copyright © 2019 PURAM SAI SURAJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
